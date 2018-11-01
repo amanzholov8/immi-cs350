38dfc10fd565b226effbd2e83abfdce05660109d
Anuar <anuartb@gmail.com>
2018-11-02 00:20:40 +0900

    init Currency support for database

--- app/src/main/java/com/immi/thebrogrammers/immi/ImmIDatabase.kt
+++ app/src/main/java/com/immi/thebrogrammers/immi/ImmIDatabase.kt
@@ -1,3 +1,16 @@
 package com.immi.thebrogrammers.immi
 
-class ImmIDatabase
+class ImmIDatabase(var curr_map: Map<String, Currency>) {
+
+    fun findCurrencyByCode(curr_code: String): Currency? {
+        if (curr_map.containsKey(curr_code)) {
+            return curr_map[curr_code]
+        } else {
+            throw Exception("Currency code ${curr_code} doesn't exist")
+        }
+    }
+
+    fun convertCurrencies(from: Currency, to: Currency) {
+
+    }
+}
