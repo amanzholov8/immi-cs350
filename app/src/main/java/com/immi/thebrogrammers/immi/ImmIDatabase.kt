5ce6c62f52d3d0771036c16130507e657b25ba81
Anuar <anuartb@gmail.com>
2018-11-03 17:05:41 +0900

    init the design of main activity

--- app/src/main/java/com/immi/thebrogrammers/immi/ImmIDatabase.kt
+++ app/src/main/java/com/immi/thebrogrammers/immi/ImmIDatabase.kt
@@ -8,187 +8,187 @@ class ImmIDatabase {
   val curr_map = mutableMapOf<String, Currency>()
   val qindices = arrayListOf<QIndex>()
 
-    init {
-        //NOTE: Init the arrays with some dummy data
+  init {
+    //NOTE: Init the arrays with some dummy data
 
-        //Currencies' initialization
-        currencies.add(
-                Currency(
-                        cur_name = "USD",
-                        usd_to_cur = 1.0,
-                        eur_to_cur = 1.14
-                ))
-        currencies.add(
-                Currency(
-                        cur_name = "AED",
-                        usd_to_cur = 3.6731809999999996,
-                        eur_to_cur = 4.190760928470784
-                ))
-        currencies.add(
-                Currency(
-                        cur_name = "KZT",
-                        usd_to_cur = 370.490964,
-                        eur_to_cur = 422.69603819759385
-                ))
-        currencies.add(
-                Currency(
-                        cur_name = "RUB",
-                        usd_to_cur = 65.7162,
-                        eur_to_cur = 74.97612650385912
-                ))
-        currencies.add(
-                Currency(
-                        cur_name = "KRW",
-                        usd_to_cur = 1123.83,
-                        eur_to_cur = 1282.1864357469237
-                ))
-        currencies.add(
-                Currency(
-                        cur_name = "AUD",
-                        usd_to_cur = 1.381104,
-                        eur_to_cur = 1.5757123543203329
-                ))
-        currencies.add(
-                Currency(
-                        cur_name = "CAD",
-                        usd_to_cur = 1.307928,
-                        eur_to_cur = 1.4922252836582068
-                ))
-        currencies.add(
-                Currency(
-                        cur_name = "AZN",
-                        usd_to_cur = 1.7025,
-                        eur_to_cur = 1.9423955641503943
-                ))
-
-
-        //Countries initialization
-        countries.add(
-                Country(
-                        geo_name = "USA",
-                        lat = 0f,
-                        lon = 0f,
-                        currency = currencies[0]
-                ))
-        countries.add(
-                Country(
-                        geo_name = "United Arab Emirates",
-                        lat = 0f,
-                        lon = 0f,
-                        currency = currencies[1]
-                ))
-        countries.add(
-                Country(
-                        geo_name = "Kazakhstan",
-                        lat = 0f,
-                        lon = 0f,
-                        currency = currencies[2]
-                ))
-        countries.add(
-                Country(
-                        geo_name = "Russia",
-                        lat = 0f,
-                        lon = 0f,
-                        currency = currencies[3]
-                ))
-        countries.add(
-                Country(
-                        geo_name = "South Korea",
-                        lat = 0f,
-                        lon = 0f,
-                        currency = currencies[4]
-                ))
-        countries.add(
-                Country(
-                        geo_name = "Australia",
-                        lat = 0f,
-                        lon = 0f,
-                        currency = currencies[5]
-                ))
-        countries.add(
-                Country(
-                        geo_name = "Canada",
-                        lat = 0f,
-                        lon = 0f,
-                        currency = currencies[6]
-                ))
-        countries.add(
-                Country(
-                        geo_name = "Azerbaijan",
-                        lat = 0f,
-                        lon = 0f,
-                        currency = currencies[7]
-                ))
+    //Currencies' initialization
+    currencies.add(
+      Currency(
+        cur_name = "USD",
+        usd_to_cur = 1.0,
+        eur_to_cur = 1.14
+      ))
+    currencies.add(
+      Currency(
+        cur_name = "AED",
+        usd_to_cur = 3.6731809999999996,
+        eur_to_cur = 4.190760928470784
+      ))
+    currencies.add(
+      Currency(
+        cur_name = "KZT",
+        usd_to_cur = 370.490964,
+        eur_to_cur = 422.69603819759385
+      ))
+    currencies.add(
+      Currency(
+        cur_name = "RUB",
+        usd_to_cur = 65.7162,
+        eur_to_cur = 74.97612650385912
+      ))
+    currencies.add(
+      Currency(
+        cur_name = "KRW",
+        usd_to_cur = 1123.83,
+        eur_to_cur = 1282.1864357469237
+      ))
+    currencies.add(
+      Currency(
+        cur_name = "AUD",
+        usd_to_cur = 1.381104,
+        eur_to_cur = 1.5757123543203329
+      ))
+    currencies.add(
+      Currency(
+        cur_name = "CAD",
+        usd_to_cur = 1.307928,
+        eur_to_cur = 1.4922252836582068
+      ))
+    currencies.add(
+      Currency(
+        cur_name = "AZN",
+        usd_to_cur = 1.7025,
+        eur_to_cur = 1.9423955641503943
+      ))
 
 
+    //Countries initialization
+    countries.add(
+      Country(
+        geo_name = "USA",
+        lat = 0f,
+        lon = 0f,
+        currency = currencies[0]
+      ))
+    countries.add(
+      Country(
+        geo_name = "United Arab Emirates",
+        lat = 0f,
+        lon = 0f,
+        currency = currencies[1]
+      ))
+    countries.add(
+      Country(
+        geo_name = "Kazakhstan",
+        lat = 0f,
+        lon = 0f,
+        currency = currencies[2]
+      ))
+    countries.add(
+      Country(
+        geo_name = "Russia",
+        lat = 0f,
+        lon = 0f,
+        currency = currencies[3]
+      ))
+    countries.add(
+      Country(
+        geo_name = "South Korea",
+        lat = 0f,
+        lon = 0f,
+        currency = currencies[4]
+      ))
+    countries.add(
+      Country(
+        geo_name = "Australia",
+        lat = 0f,
+        lon = 0f,
+        currency = currencies[5]
+      ))
+    countries.add(
+      Country(
+        geo_name = "Canada",
+        lat = 0f,
+        lon = 0f,
+        currency = currencies[6]
+      ))
+    countries.add(
+      Country(
+        geo_name = "Azerbaijan",
+        lat = 0f,
+        lon = 0f,
+        currency = currencies[7]
+      ))
 
 
 
-        cities.add(
-                City(
-                        geo_name = "Seattle",
-                        lat = 34.22f,
-                        lon = 12.1f,
-                        country = countries[0]
-                ))
-
-        cities.add(
-                City(
-                        geo_name = "Dubai",
-                        lat = 25.2048493f,
-                        lon = 55.2707828f,
-                        country = countries[1]
-                ))
-
-        cities.add(
-                City(
-                        geo_name = "Almaty",
-                        lat = 43.2220146f,
-                        lon = 76.8512485f,
-                        country = countries[2]
-                ))
-
-        cities.add(
-                City(
-                        geo_name = "Moscow",
-                        lat = 55.755826f,
-                        lon = 37.6173f,
-                        country = countries[3]
-                ))
-
-        cities.add(
-                City(
-                        geo_name = "Seoul",
-                        lat = 37.566535f,
-                        lon = 126.9779692f,
-                        country = countries[4]
-                ))
-
-        cities.add(
-                City(
-                        geo_name = "Melbourne",
-                        lat = -37.814251f,
-                        lon = 144.963169f,
-                        country = countries[5]
-                ))
-
-        cities.add(
-                City(
-                        geo_name = "Ottawa",
-                        lat = 45.4215296f,
-                        lon = -75.69719309999999f,
-                        country = countries[6]
-                ))
 
 
-        cities.add(
-                City(
-                        geo_name = "Baku",
-                        lat = 40.40926169999999f,
-                        lon = 49.8670924f,
-                        country = countries[7]
-                ))
-    }
+    cities.add(
+      City(
+        geo_name = "Seattle",
+        lat = 34.22f,
+        lon = 12.1f,
+        country = countries[0]
+      ))
+
+    cities.add(
+      City(
+        geo_name = "Dubai",
+        lat = 25.2048493f,
+        lon = 55.2707828f,
+        country = countries[1]
+      ))
+
+    cities.add(
+      City(
+        geo_name = "Almaty",
+        lat = 43.2220146f,
+        lon = 76.8512485f,
+        country = countries[2]
+      ))
+
+    cities.add(
+      City(
+        geo_name = "Moscow",
+        lat = 55.755826f,
+        lon = 37.6173f,
+        country = countries[3]
+      ))
+
+    cities.add(
+      City(
+        geo_name = "Seoul",
+        lat = 37.566535f,
+        lon = 126.9779692f,
+        country = countries[4]
+      ))
+
+    cities.add(
+      City(
+        geo_name = "Melbourne",
+        lat = -37.814251f,
+        lon = 144.963169f,
+        country = countries[5]
+      ))
+
+    cities.add(
+      City(
+        geo_name = "Ottawa",
+        lat = 45.4215296f,
+        lon = -75.69719309999999f,
+        country = countries[6]
+      ))
+
+
+    cities.add(
+      City(
+        geo_name = "Baku",
+        lat = 40.40926169999999f,
+        lon = 49.8670924f,
+        country = countries[7]
+      ))
+  }
 
   fun findCurrencyByCode(curr_code: String): Currency? {
     if (curr_map.containsKey(curr_code)) {
@@ -199,13 +199,14 @@ class ImmIDatabase {
   }
 
 
-  fun convertCurrencies(from: String, to: String, amount:Double): Double {
-      val fromCurr = findCurrencyByCode(from)!!
-      val amountToUsd = amount / fromCurr.usd_to_cur
-      val toCurr = findCurrencyByCode(to)!!
-      val result = toCurr.usd_to_cur * amountToUsd
-      return result
+  fun convertCurrencies(from: String, to: String, amount: Double): Double {
+    val fromCurr = findCurrencyByCode(from)!!
+    val amountToUsd = amount / fromCurr.usd_to_cur
+    val toCurr = findCurrencyByCode(to)!!
+    val result = toCurr.usd_to_cur * amountToUsd
+    return result
   }
+
   fun getQIndexByName(qname: String) {
 
   }
