aab09023aa9052a6a89c0b4ff38b422205e711ca
Anuar <anuartb@gmail.com>
2018-11-07 22:03:13 +0900

    init the design of landing page

--- app/src/main/java/com/immi/thebrogrammers/immi/MainActivity.kt
+++ app/src/main/java/com/immi/thebrogrammers/immi/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
-import android.widget.ArrayAdapter
-import kotlinx.android.synthetic.main.activity_main.*
 
 val db = ImmIDatabase()
 
@@ -15,9 +13,7 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
-    val geoNames = arrayListOf<String>()
-
-
+    /*val geoNames = arrayListOf<String>()
 
     geoNames += db.cities.map({ c -> c.geo_name }).toTypedArray()
     //geoNames += db.countries.map({c -> c.geo_name}).toTypedArray()
@@ -25,7 +21,7 @@ class MainActivity : AppCompatActivity() {
       this,
       android.R.layout.simple_list_item_1,
       geoNames)
-    searchBar.setAdapter(adapter)
+    searchBar.setAdapter(adapter)*/
   }
 
   fun showInfo(view: View) {
