a3c09a78ce45cbce6eb6a660b38432fd228874d2
BaurTokenov <mid1or1feed@gmail.com>
2018-11-17 23:58:51 +0900

    Improved compareTwoCities method with keyboard disappear auto-fill

--- app/src/main/java/com/immi/thebrogrammers/immi/CompareCities.kt
+++ app/src/main/java/com/immi/thebrogrammers/immi/CompareCities.kt
@@ -1,19 +1,20 @@
 package com.immi.thebrogrammers.immi
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.ArrayAdapter
-import android.widget.EditText
-import android.widget.Spinner
 import kotlinx.android.synthetic.main.activity_compare_cities.*
+
 
 class CompareCities : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_compare_cities)
-    val spinner: Spinner = findViewById<Spinner>(R.id.indexSpinner)
+
 // Create an ArrayAdapter using the string array and a default spinner layout
     ArrayAdapter.createFromResource(
       this,
@@ -23,19 +24,29 @@ class CompareCities : AppCompatActivity() {
       // Specify the layout to use when the list of choices appears
       adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
       // Apply the adapter to the spinner
-      spinner.adapter = adapter
+      indexSpinner.adapter = adapter
     }
+    val geoNames = arrayListOf<String>()
+
+    geoNames += db.cities.map({ c -> c.geo_name }).toTypedArray()
+    //geoNames += db.countries.map({c -> c.geo_name}).toTypedArray()
+    val adapter = ArrayAdapter<String>(
+      this,
+      android.R.layout.simple_list_item_1,
+      geoNames)
+    inputCity1.setAdapter(adapter)
+    inputCity2.setAdapter(adapter)
+
   }
 
   fun compareTwoCities(view: View) {
 
-    val cityInput1: EditText = findViewById<EditText>(R.id.inputCity1)
-    val cityInput2: EditText = findViewById<EditText>(R.id.inputCity2)
-    val cityName1 = cityInput1.text.toString()
-    val cityName2 = cityInput2.text.toString()
+
+    val cityName1 = inputCity1.text.toString()
+    val cityName2 = inputCity2.text.toString()
     println(cityName1)
     println(cityName2)
-    val spinner: Spinner = findViewById<Spinner>(R.id.indexSpinner)
+
     var textSpinner = indexSpinner.selectedItem.toString()
     val indMap = mapOf("Healthcare quality" to "health_care_index",
       "Cost of groceries" to "groceries_index", "Pollution level" to "pollution_index",
@@ -48,5 +59,7 @@ class CompareCities : AppCompatActivity() {
     val qindex = db.getQIndexByName(qindexString!!)
     val ans = city1?.compareCities(city2!!, qindex!!)
     answerText.text = ans
+    val mgr = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    mgr.hideSoftInputFromWindow(inputCity2.windowToken, 0)
   }
 }
