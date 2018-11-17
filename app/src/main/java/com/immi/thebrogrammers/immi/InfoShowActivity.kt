8c1836b2bbde48adb910830e892704d12fa1741e
miraskab <miras.kab@gmail.com>
2018-11-17 22:05:35 +0900

    Implemented the sidebar

--- app/src/main/java/com/immi/thebrogrammers/immi/InfoShowActivity.kt
+++ app/src/main/java/com/immi/thebrogrammers/immi/InfoShowActivity.kt
@@ -2,6 +2,9 @@ package com.immi.thebrogrammers.immi
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.View
@@ -9,13 +12,22 @@ import android.view.View
 class InfoShowActivity : AppCompatActivity() {
 
   lateinit var toolbar: Toolbar
-
+  lateinit var drawerLayout: DrawerLayout
+  lateinit var navigationView: NavigationView
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_info_show)
+    setContentView(R.layout.navigation_drawer)
 
     toolbar = findViewById<Toolbar>(R.id.toolbar)
     setSupportActionBar(toolbar)
+
+    drawerLayout = findViewById(R.id.drawer_layout)
+    navigationView = findViewById(R.id.navigation_view)
+
+    var toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer)
+    drawerLayout.setDrawerListener(toggle)
+
+    toggle.syncState()
   }
 
   fun nearbyCities(view: View) {
--- app/src/main/java/com/immi/thebrogrammers/immi/MainActivity.kt
+++ app/src/main/java/com/immi/thebrogrammers/immi/MainActivity.kt
@@ -2,34 +2,28 @@ package com.immi.thebrogrammers.immi
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
 import android.util.Log
 import android.view.View
 
 val db = ImmIDatabase()
-
 class MainActivity : AppCompatActivity() {
 
-  lateinit var toolbar: Toolbar
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+        /*val geoNames = arrayListOf<String>()
 
-    toolbar = findViewById<Toolbar>(R.id.toolbar)
-    setSupportActionBar(toolbar)
-    /*val geoNames = arrayListOf<String>()
+        geoNames += db.cities.map({ c -> c.geo_name }).toTypedArray()
+        //geoNames += db.countries.map({c -> c.geo_name}).toTypedArray()
+        val adapter = ArrayAdapter<String>(
+          this,
+          android.R.layout.simple_list_item_1,
+          geoNames)
+        searchBar.setAdapter(adapter)*/
+    }
 
-    geoNames += db.cities.map({ c -> c.geo_name }).toTypedArray()
-    //geoNames += db.countries.map({c -> c.geo_name}).toTypedArray()
-    val adapter = ArrayAdapter<String>(
-      this,
-      android.R.layout.simple_list_item_1,
-      geoNames)
-    searchBar.setAdapter(adapter)*/
-  }
-
-  fun showInfo(view: View) {
-    Log.d("debug", "go to info page")
-  }
+    fun showInfo(view: View) {
+        Log.d("debug", "go to info page")
+    }
 }
