ed6686e0fa51ca0e81ff7be13a747e82cbe7d103
BaurTokenov <mid1or1feed@gmail.com>
2018-11-17 22:08:26 +0900

    Improved compareTwoCities method

--- app/src/main/java/com/immi/thebrogrammers/immi/CompareCities.kt
+++ app/src/main/java/com/immi/thebrogrammers/immi/CompareCities.kt
@@ -3,7 +3,9 @@ package com.immi.thebrogrammers.immi
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.EditText
+import android.widget.Spinner
 import kotlinx.android.synthetic.main.activity_compare_cities.*
 
 class CompareCities : AppCompatActivity() {
@@ -11,6 +13,18 @@ class CompareCities : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_compare_cities)
+    val spinner: Spinner = findViewById<Spinner>(R.id.indexSpinner)
+// Create an ArrayAdapter using the string array and a default spinner layout
+    ArrayAdapter.createFromResource(
+      this,
+      R.array.indexArray,
+      android.R.layout.simple_spinner_item
+    ).also { adapter ->
+      // Specify the layout to use when the list of choices appears
+      adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+      // Apply the adapter to the spinner
+      spinner.adapter = adapter
+    }
   }
 
   fun compareTwoCities(view: View) {
@@ -21,12 +35,18 @@ class CompareCities : AppCompatActivity() {
     val cityName2 = cityInput2.text.toString()
     println(cityName1)
     println(cityName2)
+    val spinner: Spinner = findViewById<Spinner>(R.id.indexSpinner)
+    var textSpinner = indexSpinner.selectedItem.toString()
+    val indMap = mapOf("Healthcare quality" to "health_care_index",
+      "Cost of groceries" to "groceries_index", "Pollution level" to "pollution_index",
+      "Crime level" to "crime_index", "Traffic" to "traffic_index")
+    val qindexString = indMap[textSpinner]
     //textCity1.text = city1
     //textCity2.text = city2
     val city1 = db.getCityByName(cityName1)
     val city2 = db.getCityByName(cityName2)
-    val qindex = db.qindices[0]
-    val ans = city1?.compareCities(city2!!, qindex)
+    val qindex = db.getQIndexByName(qindexString!!)
+    val ans = city1?.compareCities(city2!!, qindex!!)
     answerText.text = ans
   }
 }
